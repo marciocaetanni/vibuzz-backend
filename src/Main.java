375dfad732ea4d0d92d5f50199c13fe3eefa7166
marciocaetanni <marciolorenzo1@hotmail.com>
2026-01-20 16:28:31 -0300

    Instantiating an object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import model.Client;
+import model.Establishment;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Vibuzz backend iniciado!");
+        
+        Client client = new Client(
+            1L, 
+            "Márcio", 
+            "marcio@marcio.com", 
+            "1234567890", 
+            true
+        );
+
+        client.addCashback(20.0);
+
+        System.out.println("Cliente: " + client.getName());
+        System.out.println("Cashback: " + client.getCashbackBalance());
+
+        Establishment bar = new Establishment(
+            2L, 
+            "Chapéu de Javali Tabacaria & Pub", 
+            "contato@chapeudejavali.com", 
+            "0987654321", 
+            "Chapéu de Javali", 
+            "12.183.895/0001-00"
+        );
+
+        System.out.println("Estabelecimento: " + bar.getbusinessName());
     }
 }
